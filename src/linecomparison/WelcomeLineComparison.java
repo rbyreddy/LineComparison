981d64d530fcf42a61cafc1bdfd634c8a4aaf30d
Rakesh <byreddyrakesh41@gmail.com>
2022-10-18 17:50:32 +0530

    ADDED: Display Message for Line Comparison Computation

---
+++ src/linecomparison/WelcomeLineComparison.java
@@ -0,0 +1,7 @@
+package linecomparison;
+
+public class WelcomeLineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation");
+    }
+}
